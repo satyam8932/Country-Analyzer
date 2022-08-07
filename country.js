d649615675f6fd036e2d8a74136ac02b8f278b72
Satyam Singh <82116707+satyam8932@users.noreply.github.com>
2022-08-07 23:35:25 +0530

    Add files via upload

---
+++ country.js
@@ -0,0 +1,50 @@
+// URL Grabber
+let search = document.getElementById("searchbox");
+let searchButton = document.getElementById("searchbtn");
+
+
+// Firing The Event 
+
+searchButton.addEventListener("click", function () {
+
+    // URL Align
+
+    let countryName = search.value;
+    let finalUrl = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+    // console.log(finalUrl);
+
+    // Initializing API
+
+    let request = new XMLHttpRequest();
+    request.open("GET", finalUrl);
+    request.send();
+
+    // Making Container
+    let dataContainer = document.getElementById('data');
+
+    // Getting Data 
+
+    request.addEventListener("load", function () {
+        //  console.log(this.responseText);
+        let data = JSON.parse(this.responseText);
+        console.log(data)
+
+        let htmlData = `
+        <div class="image" id="image">
+                <img src="${data[0].flags.svg}" alt="Country Flag" id="innerimage">
+            </div>
+            <div class="content" id="content">
+                <ul class="contents" id="contents">
+                    <li class="item" id="item1">${data[0].name.common}</li>
+                    <li class="item" id="item2"><b>Capital :</b> ${data[0].capital[0]}</li>
+                    <li class="item" id="item3"><b>Population :</b> ${data[0].population}</li>
+                    <li class="item" id="item4"><b>Currency :</b> ${Object.keys(data[0].currencies)[0]}</li>
+                    <li class="item" id="item5"><b>Language :</b> ${Object.values(data[0].languages)}</li>
+                </ul>
+            </div>
+        `
+        // dataContainer.insertAdjacentHTML('beforeb',htmlData);
+        dataContainer.innerHTML = htmlData
+        
+    });
+});
